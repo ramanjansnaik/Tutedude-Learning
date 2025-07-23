926002cbc246ab04fbf395e83d566fd1773412ff
ramanjansnaik <ramanjansnaik@gmail.com>
2025-07-23 21:43:29 +0530

    Add files via upload

---
+++ assignment 2 task 1.py
@@ -0,0 +1,13 @@
+#Takes an integer input from the user.
+#2. 	Checks whether the number is even or odd using an if-else statement.
+#3. 	Displays the result accordingly.
+
+
+
+n = int(input("Enter the number: "))
+
+
+if n % 2 ==0:
+    print(n," is an even number")
+else:
+    print(n,"is odd number")
---
+++ assignment 2 task 2.py
@@ -0,0 +1,13 @@
+#Uses a for loop to iterate over numbers from 1 to 50.
+#2.   Calculates the sum of all integers in this range.
+#3.   Displays the final sum.
+
+
+total_sum = 0
+
+for i in range(1,51):
+    total_sum += i
+
+print("the sum of the numbers 1 to 50 is: ", total_sum)
+
+
