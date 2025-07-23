0b1a1a25f2645cf516a6f11628df5870f6f5b97e
ramanjansnaik <ramanjansnaik@gmail.com>
2025-07-23 20:33:31 +0530

    Assignment on - Basic Python Coding task 1

---
+++ assignment1.py
@@ -0,0 +1,17 @@
+# task 1 1.  Takes two numbers as input from the user.
+# 2.  Performs the basic mathematical operations on these two numbers:
+# o	Addition
+# o	Subtraction
+# o	Multiplication
+# o	Division
+# 3.  Displays the results of each operation on the screen.
+
+
+x = int(input("Please Enter the first Number : "))
+y = int(input("please enter the second number: "))
+
+print ("addition of the numbers is :", x + y)
+print ("Subraction of the numbers is: ", x - y)
+print ("multiplication of the numbers is: ", x * y)
+print ("division of the numbers is: ", x / y)
+
